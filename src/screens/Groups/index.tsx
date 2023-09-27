c1465cd34b7aab8c0c1b598f44848b561ba13140
Igor Cassolli <37945750+IgorCassolli@users.noreply.github.com>
2023-09-27 19:47:02 -0300

    Starting with Async Storage

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import { FlatList } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
+import { groupsGetAll } from '@storage/group/groupsGetAll';
 
 import { Container } from './styles';
 
@@ -10,6 +11,7 @@ import { GroupCard } from '@components/GroupCard';
 import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
 
+
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
 
@@ -18,6 +20,19 @@ export function Groups() {
   function handleNewGroup(){
     navigation.navigate('new')
   }
+
+  async function fetchGroups(){
+    try {
+      const data = await groupsGetAll();
+      setGroups(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useFocusEffect(useCallback(() => {
+    fetchGroups();
+  }, []));
 
   return (
     <Container>
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react';
-import { Header } from "@components/Header";
+import { groupCreate } from '@storage/group/groupCreate';
+import { useNavigation } from "@react-navigation/native";
+
 import { Container, Content, Icon } from "./styles";
+
+import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
-import { useNavigation } from "@react-navigation/native";
 
 export function NewGroup(){
 
@@ -12,8 +15,13 @@ export function NewGroup(){
 
     const navigation = useNavigation();
 
-    function handleNew(){
-        navigation.navigate('players', { group });
+    async function handleNew(){
+        try {
+            await groupCreate(group);
+            navigation.navigate('players', { group });
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     return (
